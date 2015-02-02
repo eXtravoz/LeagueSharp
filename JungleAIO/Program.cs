049906fb72dae7fa4abda17c7fec0978dc21d8c5
eXtravoz <eXtravoz@outlook.com>
2015-02-02 17:44:32 -0200

    Update Program.cs

--- JungleAIO/Program.cs
+++ JungleAIO/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -33,6 +33,12 @@ namespace JungleAIO
                     
                 case "Pantheon":
                     new Pantheon();
+                    Game.PrintChat("Loaded - [JungleAIO]");
+                    Game.PrintChat("[JungleAIO] - " + ObjectManager.Player.ChampionName);
+                    break;
+
+                case "Nautilus":
+                    new Nautilus();
                     Game.PrintChat("Loaded - [JungleAIO]");
                     Game.PrintChat("[JungleAIO] - " + ObjectManager.Player.ChampionName);
                     break;
