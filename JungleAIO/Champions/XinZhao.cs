797c0ffc0c6dd668ba28efc6fbc3dfee8e4e5b98
eXtravoz <eXtravoz@outlook.com>
2015-02-03 13:59:21 -0200

    Code Update && No Dictionaries

--- JungleAIO/Champions/XinZhao.cs
+++ JungleAIO/Champions/XinZhao.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Dynamic;
 using System.Linq;
@@ -23,25 +23,28 @@ namespace JungleAIO.Champions
         public static Obj_AI_Hero Player;
         public static Orbwalking.Orbwalker Orbwalker;
         public static Menu Config;
+        public static Spell Q, W, E, R;
 
         public static SpellSlot IgniteSlot;
 
-        private readonly Dictionary<SpellSlot, Spell> _spells = new Dictionary<SpellSlot, Spell>
-        {
-            {SpellSlot.Q, new Spell(SpellSlot.Q, 0)},
-	        {SpellSlot.W, new Spell(SpellSlot.W, 0)},
-	        {SpellSlot.E, new Spell(SpellSlot.E, 650)},
-	        {SpellSlot.R, new Spell(SpellSlot.R, 500)}
-        };
+        public static Items.Item Biscuit = new Items.Item(2010, 10);
+        public static Items.Item HPpot = new Items.Item(2003, 10);
+        public static Items.Item Flask = new Items.Item(2041, 10);
+        
 
         public XinZhao()
         {
-            CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
+            Game_OnGameLoad();
         }
 
-        private void Game_OnGameLoad(EventArgs args)
-        {                      
+        private static void Game_OnGameLoad()
+        {
 
+            Q = new Spell(SpellSlot.Q, 0f);
+            W = new Spell(SpellSlot.W, 0f);
+            E = new Spell(SpellSlot.E, 625f);
+            R = new Spell(SpellSlot.R, 500f);
+            
             Config = new Menu("Xin Zhao", "XinZhao", true);
 
             Config.AddSubMenu(new Menu("Orbwalking", "Orbwalking"));
@@ -51,26 +54,26 @@ namespace JungleAIO.Champions
             TargetSelector.AddToMenu(SimpleTs);
             Config.AddSubMenu(SimpleTs);
 
-            var c = Config.AddSubMenu(new Menu("Combo", "Combo"));
+            var c = Config.AddSubMenu(new Menu("Combo Settings", "Combo"));
             c.SubMenu("Combo").AddItem(new MenuItem("UseQCombo", "Use Q").SetValue(true));
             c.SubMenu("Combo").AddItem(new MenuItem("UseWCombo", "Use W").SetValue(true));
             c.SubMenu("Combo").AddItem(new MenuItem("UseECombo", "Use E").SetValue(true));
             c.SubMenu("Combo").AddItem(new MenuItem("igniteCombo", "Use Ignite if Killable").SetValue(true));
             c.SubMenu("Combo").AddItem(new MenuItem("ComboActive", "Combo!").SetValue(new KeyBind(Config.Item("Orbwalk").GetValue<KeyBind>().Key, KeyBindType.Press)));
 
-            var f = Config.AddSubMenu(new Menu("Farm", "Farm"));
-            f.SubMenu("LaneClear").AddItem(new MenuItem("UseQFarm", "Use Q").SetValue(true));
-            f.SubMenu("LaneClear").AddItem(new MenuItem("UseWFarm", "Use W").SetValue(true));
-            f.SubMenu("LaneClear").AddItem(new MenuItem("UseEFarm", "Use E").SetValue(true));
-            f.SubMenu("LaneClear").AddItem(new MenuItem("FarmActive", "Farm!").SetValue(new KeyBind("V".ToCharArray()[0], KeyBindType.Press)));
+            var f = Config.AddSubMenu(new Menu("LaneClear", "Farm"));
+            f.SubMenu("Farm").AddItem(new MenuItem("UseQFarm", "Use Q").SetValue(true));
+            f.SubMenu("Farm").AddItem(new MenuItem("UseWFarm", "Use W").SetValue(true));
+            f.SubMenu("Farm").AddItem(new MenuItem("UseEFarm", "Use E").SetValue(true));
+            f.SubMenu("Farm").AddItem(new MenuItem("FarmActive", "Farm!").SetValue(new KeyBind("V".ToCharArray()[0], KeyBindType.Press)));
 
             var l = Config.AddSubMenu(new Menu("JungleClear", "JClear"));
-            l.SubMenu("JungleClear").AddItem(new MenuItem("UseQJC", "Use Q").SetValue(true));
-            l.SubMenu("JungleClear").AddItem(new MenuItem("UseWJC", "Use W").SetValue(true));
-            l.SubMenu("JungleClear").AddItem(new MenuItem("UseEJC", "Use E").SetValue(true));
-            l.SubMenu("JungleClear").AddItem(new MenuItem("JCActive", "JungleClear!").SetValue(new KeyBind("V".ToCharArray()[0], KeyBindType.Press)));
+            l.SubMenu("JClear").AddItem(new MenuItem("UseQJC", "Use Q").SetValue(true));
+            l.SubMenu("JClear").AddItem(new MenuItem("UseWJC", "Use W").SetValue(true));
+            l.SubMenu("JClear").AddItem(new MenuItem("UseEJC", "Use E").SetValue(true));
+            l.SubMenu("JClear").AddItem(new MenuItem("JCActive", "JungleClear!").SetValue(new KeyBind("V".ToCharArray()[0], KeyBindType.Press)));
 
-            var i = Config.AddSubMenu(new Menu("Items", "Items"));
+            var i = Config.AddSubMenu(new Menu("Items Settings", "Items"));
             l.SubMenu("Items").AddItem(new MenuItem("UseHydra", "Use Hydra").SetValue(true));
             l.SubMenu("Items").AddItem(new MenuItem("UseTiamat", "Use Tiamat").SetValue(true));
 
@@ -79,12 +82,20 @@ namespace JungleAIO.Champions
             k.SubMenu("KillSteal").AddItem(new MenuItem("UseEKs", "Use E").SetValue(true));
             k.SubMenu("KillSteal").AddItem(new MenuItem("UseRKs", "Use R").SetValue(true));
 
-            var m = Config.AddSubMenu(new Menu("Misc", "Misc"));
+            var m = Config.AddSubMenu(new Menu("Misc Settings", "Misc"));
             m.SubMenu("Misc").AddItem(new MenuItem("SpellInterrupt", "Interrupt Spells").SetValue(true));
             m.SubMenu("Misc").AddItem(new MenuItem("GapCloser", "Anti-GapCloser").SetValue(true));
             // m.SubMenu("Misc").AddItem(new MenuItem("UsePackets", "Packets").SetValue(true));
 
-            var d = Config.AddSubMenu(new Menu("Drawings", "Drawings"));
+            var p = Config.AddSubMenu(new Menu("AutoPot", "AutoPot"));
+            p.SubMenu("AutoPot").AddItem(new MenuItem("AutoPot", "AutoPot enabled").SetValue(true));
+            p.SubMenu("AutoPot").AddItem(new MenuItem("AP_H", "Health Pot").SetValue(true));
+            p.SubMenu("AutoPot").AddItem(new MenuItem("AP_M", "Mana Pot").SetValue(true));
+            p.SubMenu("AutoPot").AddItem(new MenuItem("AP_H_Per", "Health Pot %").SetValue(new Slider(35, 1)));
+            p.SubMenu("AutoPot").AddItem(new MenuItem("AP_M_Per", "Mana Pot %").SetValue(new Slider(35, 1)));
+            p.SubMenu("AutoPot").AddItem(new MenuItem("AP_Ign", "Auto pot when ignite").SetValue(true));
+
+            var d = Config.AddSubMenu(new Menu("Drawings Settings", "Drawings"));
             d.SubMenu("Drawings").AddItem(new MenuItem("ERange", "E Range").SetValue(true));
             d.SubMenu("Drawings").AddItem(new MenuItem("RRange", "R Range").SetValue(true));
 
@@ -98,47 +109,92 @@ namespace JungleAIO.Champions
 
         }
 
-        private void AntiGapcloserOnOnEnemyGapcloser(ActiveGapcloser gapcloser)
+        private static void AntiGapcloserOnOnEnemyGapcloser(ActiveGapcloser gapcloser)
         {
             if (Config.Item("GapCloser").GetValue<bool>())
             {
-                if (_spells[SpellSlot.R].IsReady())
+                if (R.IsReady())
                 {
-                    _spells[SpellSlot.R].Cast(gapcloser.Sender);
+                    R.Cast(gapcloser.Sender);
                 }
             }
         }
 
 
-        private void Interrupter_OnPossibleToInterrupt(Obj_AI_Hero unit, InterruptableSpell spell)
+        private static void Interrupter_OnPossibleToInterrupt(Obj_AI_Hero unit, InterruptableSpell spell)
         {
 
             if (Config.Item("SpellInterrupt").GetValue<bool>())
             {
-                if (Player.Distance(unit) < _spells[SpellSlot.R].Range && _spells[SpellSlot.R].IsReady())
+                if (Player.Distance(unit) < R.Range && R.IsReady())
                 {
-                    _spells[SpellSlot.R].Cast();
+                    R.Cast();
+                }
+            }
+        }
+
+        private void AutoPot()
+        {
+            if (Config.Item("AutoPot").GetValue<bool>())
+            {
+                if (Player.HasBuff("summonerdot") || Player.HasBuff("MordekaiserChildrenOfTheGrave"))
+                {
+                    if (!Player.InFountain())
+                    {
+                        if (Items.HasItem(Biscuit.Id) && Items.CanUseItem(Biscuit.Id) &&
+                            !Player.HasBuff("ItemMiniRegenPotion"))
+                        {
+                            Biscuit.Cast(Player);
+                        }
+                        else if (Items.HasItem(HPpot.Id) && Items.CanUseItem(HPpot.Id) &&
+                                 !Player.HasBuff("RegenerationPotion") && !Player.HasBuff("Health Potion"))
+                        {
+                            HPpot.Cast(Player);
+                        }
+                        else if (Items.HasItem(Flask.Id) && Items.CanUseItem(Flask.Id) &&
+                                 !Player.HasBuff("ItemCrystalFlask"))
+                        {
+                            Flask.Cast(Player);
+                        }
+                    }
+                }
+
+                if (ObjectManager.Player.HasBuff("Recall") || Player.InFountain() && Player.InShop())
+                {
+                    return;
+                }
+
+                //Health Pots
+                if (Player.Health / 100 <= Config.Item("AP_H_Per").GetValue<Slider>().Value &&
+                    !Player.HasBuff("RegenerationPotion", true))
+                {
+                    Items.UseItem(2003);
+                }
+                //Mana Pots
+                if (Player.Health / 100 <= Config.Item("AP_M_Per").GetValue<Slider>().Value &&
+                    !Player.HasBuff("FlaskOfCrystalWater", true))
+                {
+                    Items.UseItem(2004);
                 }
             }
         }
 
 
-
-        private void Drawing_OnDraw(EventArgs args)
+        private static void Drawing_OnDraw(EventArgs args)
         {
 
             var drawE = Config.Item("ERange").GetValue<bool>();
             var drawR = Config.Item("RRange").GetValue<bool>();
 
             if (drawE)
-                Render.Circle.DrawCircle(Player.Position, _spells[SpellSlot.E].Range, Color.White);
+                Render.Circle.DrawCircle(Player.Position, E.Range, Color.White);
 
             if (drawR)
-                Render.Circle.DrawCircle(Player.Position, _spells[SpellSlot.R].Range, Color.White);
+                Render.Circle.DrawCircle(Player.Position, R.Range, Color.White);
 
         }
 
-        private void Game_OnGameUpdate(EventArgs args)
+        private static void Game_OnGameUpdate(EventArgs args)
         {
             var igniteT = TargetSelector.GetTarget(600f, TargetSelector.DamageType.Physical);
             if (Config.Item("igniteCombo").GetValue<bool>())
@@ -154,30 +210,30 @@ namespace JungleAIO.Champions
 
         }
 
-        private void JungleClear()
+        private static void JungleClear()
         {
 
             var Tiamat = ItemData.Tiamat_Melee_Only.GetItem();
             var Hydra = ItemData.Ravenous_Hydra_Melee_Only.GetItem();
-            var mobs = MinionManager.GetMinions(Player.ServerPosition, _spells[SpellSlot.E].Range, MinionTypes.All, MinionTeam.Neutral, MinionOrderTypes.MaxHealth);
+            var mobs = MinionManager.GetMinions(Player.ServerPosition, E.Range, MinionTypes.All, MinionTeam.Neutral, MinionOrderTypes.MaxHealth);
             if (mobs.Count > 0)
             {
                 var minions = mobs[0];
 
-                if (Config.Item("UseQJC").GetValue<bool>() && _spells[SpellSlot.Q].IsReady() &&
-                    minions.IsValidTarget(_spells[SpellSlot.Q].Range))
+                if (Config.Item("UseQJC").GetValue<bool>() && Q.IsReady() &&
+                    minions.IsValidTarget(Q.Range))
                 {
-                    _spells[SpellSlot.Q].Cast();
+                    Q.Cast();
                 }
 
-                if (Config.Item("UseWJC").GetValue<bool>() && _spells[SpellSlot.W].IsReady())
+                if (Config.Item("UseWJC").GetValue<bool>() && W.IsReady())
                 {
-                    _spells[SpellSlot.W].Cast();
+                    W.Cast();
                 }
 
-                if (Config.Item("UseEJC").GetValue<bool>() && _spells[SpellSlot.E].IsReady() && minions.IsValidTarget(_spells[SpellSlot.E].Range))
+                if (Config.Item("UseEJC").GetValue<bool>() && E.IsReady() && minions.IsValidTarget(E.Range))
                 {
-                    _spells[SpellSlot.E].Cast(minions);
+                    E.Cast(minions);
                 }
 
                 if (Tiamat.IsOwned() && Tiamat.IsReady() && Tiamat.IsInRange(minions))
@@ -193,30 +249,30 @@ namespace JungleAIO.Champions
 
         }
 
-        private void LaneClear()
+        private static void LaneClear()
         {
 
             var Tiamat = ItemData.Tiamat_Melee_Only.GetItem();
             var Hydra = ItemData.Ravenous_Hydra_Melee_Only.GetItem();
-            var minion = MinionManager.GetMinions(Player.ServerPosition, _spells[SpellSlot.E].Range, MinionTypes.All, MinionTeam.NotAlly, MinionOrderTypes.MaxHealth);
+            var minion = MinionManager.GetMinions(Player.ServerPosition, E.Range, MinionTypes.All, MinionTeam.NotAlly, MinionOrderTypes.MaxHealth);
             if (minion.Count > 0)
             {
                 var minions = minion[0];
 
-                if (Config.Item("UseQFarm").GetValue<bool>() && _spells[SpellSlot.Q].IsReady() &&
-                    minions.IsValidTarget(_spells[SpellSlot.Q].Range))
+                if (Config.Item("UseQFarm").GetValue<bool>() && Q.IsReady() &&
+                    minions.IsValidTarget(Q.Range))
                 {
-                    _spells[SpellSlot.Q].Cast();
+                    Q.Cast();
                 }
 
-                if (Config.Item("UseWFarm").GetValue<bool>() && _spells[SpellSlot.W].IsReady())
+                if (Config.Item("UseWFarm").GetValue<bool>() && W.IsReady())
                 {
-                    _spells[SpellSlot.W].Cast();
+                    W.Cast();
                 }
 
-                if (Config.Item("UseEFarm").GetValue<bool>() && _spells[SpellSlot.E].IsReady() && minions.IsValidTarget(_spells[SpellSlot.E].Range))
+                if (Config.Item("UseEFarm").GetValue<bool>() && E.IsReady() && minions.IsValidTarget(E.Range))
                 {
-                    _spells[SpellSlot.E].Cast(minions);
+                    E.Cast(minions);
                 }
 
                 if (Tiamat.IsOwned() && Tiamat.IsReady() && Tiamat.IsInRange(minions))
@@ -231,10 +287,10 @@ namespace JungleAIO.Champions
             }
         }
 
-        private void Combo()
+        private static void Combo()
         {
 
-            var Target = TargetSelector.GetTarget(_spells[SpellSlot.E].Range, TargetSelector.DamageType.Physical);
+            var Target = TargetSelector.GetTarget(E.Range, TargetSelector.DamageType.Physical);
             var Tiamat = ItemData.Tiamat_Melee_Only.GetItem();
             var Hydra = ItemData.Ravenous_Hydra_Melee_Only.GetItem();
 
@@ -245,25 +301,25 @@ namespace JungleAIO.Champions
 
             if (Config.Item("UseECombo").GetValue<bool>())
             {
-                if (_spells[SpellSlot.E].IsReady() && Target.IsValidTarget(_spells[SpellSlot.E].Range))
+                if (E.IsReady() && Target.IsValidTarget(E.Range))
                 {
-                    _spells[SpellSlot.E].Cast(Target);
+                    E.Cast(Target);
                 }
             }
 
             if (Config.Item("UseQCombo").GetValue<bool>())
             {
-                if (_spells[SpellSlot.Q].IsReady() && Target.IsValidTarget(_spells[SpellSlot.Q].Range))
+                if (Q.IsReady() && Target.IsValidTarget(Q.Range))
                 {
-                    _spells[SpellSlot.Q].Cast();
+                    Q.Cast();
                 }
             }
 
             if (Config.Item("UseW").GetValue<bool>())
             {
-                if (_spells[SpellSlot.W].IsReady() && Target.IsValidTarget(_spells[SpellSlot.W].Range))
+                if (W.IsReady() && Target.IsValidTarget(W.Range))
                 {
-                    _spells[SpellSlot.W].Cast();
+                    W.Cast();
                 }
             }
 
@@ -283,28 +339,29 @@ namespace JungleAIO.Champions
                 }
             }
         }
-
-        private void KillSteal()
+        
+        
+        private static void KillSteal()
         {
-            foreach (var hero in ObjectManager.Get<Obj_AI_Hero>().Where(hero => hero.IsValidTarget(_spells[SpellSlot.R].Range)))
+            foreach (var hero in ObjectManager.Get<Obj_AI_Hero>().Where(hero => hero.IsValidTarget(R.Range)))
             {
                 if (Config.Item("UseRKs").GetValue<bool>())
                 {
-                    if (_spells[SpellSlot.R].IsReady() &&
-                        hero.Distance(ObjectManager.Player) <= _spells[SpellSlot.R].Range &&
+                    if (R.IsReady() &&
+                        hero.Distance(ObjectManager.Player) <= R.Range &&
                         Player.GetSpellDamage(hero, SpellSlot.E) >= hero.Health)
                     {
-                        _spells[SpellSlot.R].Cast();
+                        R.Cast();
                     }
                 }
 
                 if (Config.Item("UseEKs").GetValue<bool>())
                 {
-                    if (_spells[SpellSlot.E].IsReady() &&
-                        hero.Distance(ObjectManager.Player) <= _spells[SpellSlot.E].Range &&
+                    if (E.IsReady() &&
+                        hero.Distance(ObjectManager.Player) <= E.Range &&
                         Player.GetSpellDamage(hero, SpellSlot.E) >= hero.Health)
                     {
-                        _spells[SpellSlot.E].Cast();
+                        E.Cast();
                     }
                 }
             }
